7491d6eb124449d538e959384a930669d4b21d10
Jackie Xu <jackiexu2007@hotmail.com>
2021-09-12 14:17:04 -0400

    Shutdown delay slightly before shutting down

--- shutdown.sh
+++ shutdown.sh
@@ -3,5 +3,9 @@
 # This script shutsdown the instanace
 # Note: shutting down will STOP in the instance, not TERMINATE
 
-echo "Shutting system down now..."
+echo "Shutting system down..."
+
+# Wait for a few seconds before shutting down in case clean up scripts are still running
+# shutdown command cannot specify lengths under a minute so use sleep instead
+sleep 10
 shutsdown now
