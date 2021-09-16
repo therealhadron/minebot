2e7089bc31c8eec0b63e50de74d34deb5d8c54e8
Jackie Xu <jackiexu2007@hotmail.com>
2021-09-15 23:40:07 -0400

    Fix redundant network call

--- scripts/server_downloader.py
+++ scripts/server_downloader.py
@@ -1,5 +1,3 @@
-import requests
-
 def get_latest_version_url():
     try:
         server_project = "paper"
@@ -7,9 +5,6 @@ def get_latest_version_url():
         server_build = 259
         base_url = f"https://papermc.io/api/v2/projects/{server_project}/versions/{server_version}\
 /builds/{server_build}/downloads/{server_project}-{server_version}-{server_build}.jar"
-        r = requests.get(base_url)
-        if not r.ok:
-            return ""
 
         return base_url
     except Exception as e:
