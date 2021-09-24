9b8b6f80760157ac8cdaef2abb94901614f34b06
Jackie Xu <jackiexu2007@hotmail.com>
2021-09-23 21:05:21 -0400

    Implement properties parser with resource manager

--- scripts/properties_parser.py
+++ scripts/properties_parser.py
@@ -7,8 +7,19 @@ class PropertiesParser:
     data: dict
     def __init__(self, file_path: str):
         self.file_path = file_path
+        self.data = None
+    
+    # Gets the value of property
+    def get(self, prop: str):
+        return self.data.get(prop)
+    
+    # Sets property 'prop' to 'val'
+    def set(self, prop: str, val: str):
+        self.data[prop] = val
+
+    def __enter__(self):
         self.data = dict()
-        with open(file_path, "r") as f:
+        with open(self.file_path, "r") as f:
             for l in f:
                 line = l.strip()
                 
@@ -21,14 +32,11 @@ class PropertiesParser:
 
                 prop, val = values
                 self.data[prop] = val
-    
-    # Gets the value of property
-    def get(self, prop: str):
-        return self.data.get(prop)
-    
-    # Sets property 'prop' to 'val'
-    def set(self, prop: str, val: str):
-        self.data[prop] = val
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.save()
+        self.data = None
 
     # Saves the data to the original file
     # Comments are not saved since they're not important
--- scripts/set_server_properties.py
+++ scripts/set_server_properties.py
@@ -7,11 +7,10 @@ from properties_parser import PropertiesParser
 
 def run(properties_path: str):
     RCON_SECRET = os.environ["MC_RCON_SECRET"]
-    res = PropertiesParser(properties_path)
-
-    res.set("enable-rcon", "true")
-    res.set("rcon.password", RCON_SECRET)
-    res.save()
+    with PropertiesParser(properties_path) as props:
+        props.set("enable-rcon", "true")
+        props.set("rcon.password", RCON_SECRET)
+        
     logger.info(f"Set RCON secret to '{RCON_SECRET}' at '{properties_path}'")
 
 if __name__ == "__main__":
